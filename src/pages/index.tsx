e51ed7a8a67a9cee832a1fdc33597f4f5564e5d8
JojoWand <caio27joseph@gmail.com>
2021-01-29 23:49:00 -0300

    project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const index: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Home Page Project</title>
+      </Head>
+      <main>
+        <h1>Fate Fairies</h1>
+      </main>
+    </>
+  );
+};
+export default index;
