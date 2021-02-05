853db43a0766e26875868b803ee5ed32fee893d7
JojoWand <caio27joseph@gmail.com>
2021-02-05 20:59:59 -0300

    change database to postgres

--- prisma/migrations/20210202002914_create_spell_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Spell" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "name" TEXT NOT NULL,
-    "description" TEXT,
-    "published" BOOLEAN NOT NULL DEFAULT false
-);
